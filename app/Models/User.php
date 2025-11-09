56f18b0508295e06c092a722d5eb9b1d26ca57d2
Mau Mandi <mahameruaji123@gmail.com>
2025-11-09 23:11:36 +0700

    controller, middleware only outer

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,25 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
-    protected $fillable = ['role_id', 'name', 'email', 'password'];
+    protected $fillable = [
+        'role_id',
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     public function role()
     {
         return $this->belongsTo(Role::class);
     }
 
-    public function productions()
+    public function hasRole(string $roleName): bool
     {
-        return $this->hasMany(Production::class);
-    }
-
-    public function distributions()
-    {
-        return $this->hasMany(Distribution::class);
+        return $this->role && strtolower($this->role->name) === strtolower($roleName);
     }
 }
